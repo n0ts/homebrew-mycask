dc537a9f62d79ea34a8b010d69e2a43bdd342f0d
n0ts <me@n0ts.org>
2014-01-13 16:33:49 +0900

    Fixed a typo

--- Casks/wondershare-player.rb
+++ Casks/wondershare-player.rb
@@ -1,6 +1,6 @@
 class WondersharePlayer < Cask
   url 'http://download.wondershare.com/mac-player_full1479.dmg'
-  homepage 'http://www.wondershare.com/video-player/',
+  homepage 'http://www.wondershare.com/video-player/'
   version 'latest'
   no_checksum
 end
