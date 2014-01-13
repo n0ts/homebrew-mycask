f5ed41d750a988279b804b1d976582572c686efc
n0ts <me@n0ts.org>
2014-01-13 15:59:46 +0900

    Added casks

---
+++ Casks/pro_apps_quicktime_codecs.rb
@@ -0,0 +1,6 @@
+class ProAppsQuickTimeCodecs < Cask
+  url 'http://support.apple.com/downloads/DL1396/en_US/ProAppsQTCodecs.dmg'
+  homepage 'http://support.apple.com/kb/dl1396'
+  version '1.0.3'
+  no_checksum
+end
