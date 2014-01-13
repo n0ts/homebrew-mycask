5597a183b6cad5d247593c88478c055f8a4f5d0e
n0ts <me@n0ts.org>
2014-01-13 16:52:50 +0900

    Renamed

--- Casks/tftpserver.rb
+++ Casks/tftpserver.rb
@@ -1,6 +1,7 @@
-class Tfpserver < Cask
+class Tftpserver < Cask
   url 'http://ww2.unime.it/flr/tftpserver/TftpServer.dmg'
   homepage 'http://ww2.unime.it/flr/tftpserver/'
   version '3.4.1'
   no_checksum
+  link 'TftpServer.app'
 end
