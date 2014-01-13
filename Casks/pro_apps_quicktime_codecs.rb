58abcbceee1d44369642eb9826fd25463ffe627a
n0ts <me@n0ts.org>
2014-01-13 16:10:33 +0900

    Fixed a typo

--- Casks/pro_apps_quicktime_codecs.rb
+++ Casks/pro_apps_quicktime_codecs.rb
@@ -1,4 +1,4 @@
-class ProAppsQuickTimeCodecs < Cask
+class ProAppsQuicktimeCodecs < Cask
   url 'http://support.apple.com/downloads/DL1396/en_US/ProAppsQTCodecs.dmg'
   homepage 'http://support.apple.com/kb/dl1396'
   version '1.0.3'
