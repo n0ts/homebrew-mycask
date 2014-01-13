3ab89db71a1e3e81f2b3072c70e6a0af55cc3426
n0ts <me@n0ts.org>
2014-01-13 16:33:14 +0900

    Deleted cask

--- Casks/qr-codingen.rb
+++
@@ -1,7 +0,0 @@
-class QrCodingen < Cask
-  url 'http://micutil.com/download/QRCodingen.zip'
-  homepage 'http://micutil.com/QRCodingen/top.html'
-  version '3.1.2'
-  no_checksum
-  link 'QRこ〜でんネン！.app'
-end
