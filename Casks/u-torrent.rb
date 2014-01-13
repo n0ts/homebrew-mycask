98b4da20f297aba64e00206e0e0b427a490ca0ac
n0ts <me@n0ts.org>
2014-01-13 17:06:54 +0900

    Added casks

---
+++ Casks/u-torrent.rb
@@ -0,0 +1,7 @@
+class UTorrent < Cask
+  url 'http://download-new.utorrent.com/endpoint/utmac/os/osx/track/stable/uTorrent.dmg'
+  homepage 'http://www.utorrent.com'
+  version 'latest'
+  no_checksum
+  install 'uTorrent-Installer.app/Contents/MacOS/uTorrent-Installer'
+end
