917ede7917737e1af254db85ee8727e69a2bc03f
n0ts <me@n0ts.org>
2014-03-11 14:53:50 +0900

    Added MacZip4Win

---
+++ Casks/mac-zip-4-win.rb
@@ -0,0 +1,7 @@
+class MacZip4Win < Cask
+  url 'http://ynomura.com/files/MacZip4win_0_4_1.zip'
+  homepage 'http://ynomura.com/home/?page_id=116'
+  version '0.4.1'
+  no_checksum
+  link 'MacZip4win 0.4.1/MacZip4Win.app'
+end
