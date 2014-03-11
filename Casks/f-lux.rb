5f4cd9dae4ec031b5e9de71637f98154ccc23745
n0ts <me@n0ts.org>
2014-03-11 16:07:39 +0900

    Use beta

--- Casks/f-lux.rb
+++ Casks/f-lux.rb
@@ -1,5 +1,5 @@
 class FLux < Cask
-  url 'https://justgetflux.com/mac/Flux.zip'
+  url 'https://justgetflux.com/mac/fluxbeta.zip'
   homepage 'http://justgetflux.com'
   version 'latest'
   no_checksum
