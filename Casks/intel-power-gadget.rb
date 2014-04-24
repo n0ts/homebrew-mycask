3a0abd133e93a07a505203ed85a673aafdd2d5e5
n0ts <me@n0ts.org>
2014-04-24 12:13:34 +0900

    Deleted intel-power-gadget

--- Casks/intel-power-gadget.rb
+++
@@ -1,8 +0,0 @@
-class IntelPowerGadget < Cask
-  url 'http://software.intel.com/sites/default/files/IntelPowerGadget3.0.1.zip'
-  homepage 'http://software.intel.com/en-us/articles/intel-power-gadget-20'
-  version '3.0'
-  no_checksum
-  nested_container 'Intel%AE Power Gadget.dmg'
-  install 'Install Intel Power Gadget.pkg'
-end
