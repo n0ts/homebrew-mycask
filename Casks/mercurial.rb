59a96dccd81db7715356446747535c5d55beb447
n0ts <me@n0ts.org>
2014-04-24 12:13:45 +0900

    Update mercurial

--- Casks/mercurial.rb
+++ Casks/mercurial.rb
@@ -1,7 +1,7 @@
 class Mercurial < Cask
-  url 'http://mercurial.selenic.com/mac/binaries/Mercurial-2.9-py2.7-macosx10.9.zip'
+  url 'http://mercurial.selenic.com/mac/binaries/Mercurial-2.9.2-py2.7-macosx10.9.zip'
   homepage 'http://mercurial.selenic.com/'
-  version '2.9'
+  version '2.9.2'
   no_checksum
-  install 'mercurial-2.9_20140204-py2.7-macosx10.9/mercurial-2.9+20140204-py2.7-macosx10.9.mpkg'
+  install 'mercurial-2.9_20140204-py2.7-macosx10.9/mercurial-2.9.2-py2.7-macosx10.9.mpkg'
 end
