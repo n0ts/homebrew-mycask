3cffe188ed61aa6205c8111c7647eccc2ecf4466
n0ts <me@n0ts.org>
2014-06-04 20:13:54 +0900

    Add toolbet-mac td

---
+++ Casks/toolbelt-mac.rb
@@ -0,0 +1,7 @@
+class ToolbeltMac < Cask
+  url 'https://d3hq3slxjftf62.cloudfront.net/td-toolbelt-0.11.1.pkg'
+  homepage 'http://toolbelt.treasuredata.com/'
+  version '0.11.1'
+  no_checksum
+  install 'td-toolbelt-0.11.1.pkg'
+end
