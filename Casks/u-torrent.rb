a817d3a7150d6f22d09d290204002c79a0e825cd
Naoya Nakazawa <naoya.n@gmail.com>
2014-07-24 10:00:39 +0900

    Delete u-torrent.rb

--- Casks/u-torrent.rb
+++
@@ -1,7 +0,0 @@
-class UTorrent < Cask
-  url 'http://download-new.utorrent.com/endpoint/utmac/os/osx/track/stable/uTorrent.dmg'
-  homepage 'http://www.utorrent.com'
-  version 'latest'
-  sha256 :no_check
-  install 'uTorrent-Installer.app/Contents/MacOS/uTorrent-Installer'
-end
