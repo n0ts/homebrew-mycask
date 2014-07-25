83a6e0a9d71561b1b105b7b71ef4f06dd23ccfce
n0ts <me@n0ts.org>
2014-07-26 00:30:43 +0900

    Add uqwifi connect

---
+++ Casks/uqwifi-connect.rb
@@ -0,0 +1,7 @@
+class UqwifiConnect < Cask
+  url 'https://service.wi2.ne.jp/sw/autoLogin/MacOSX/UQWiFi-Connect-MacOSX.dmg'
+  homepage 'http://wi2.co.jp/jp/uq/connect/'
+  version 'latest'
+  sha256 :no_check
+  link 'UQ Wi-Fiコネクト.app'
+end
