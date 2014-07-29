85ac8865d68b2393941227905d1259c8306cfc2f
n0ts <me@n0ts.org>
2014-07-30 01:04:03 +0900

    Rename ProAppsCodecs

---
+++ Casks/pro-apps-codecs.rb
@@ -0,0 +1,7 @@
+class ProAppsCodecs < Cask
+  url 'http://support.apple.com/downloads/DL1396/en_US/ProAppsCodecs.dmg'
+  homepage 'http://support.apple.com/kb/DL1396'
+  version '1.0.5'
+  sha256 '59ee2945a2fdb824cdb54b037e1759ccd7ce6f572109cfd421ad85b4345b8f47'
+  install 'ProAppsCodecs.pkg'
+end
--- Casks/pro-apps-quicktime-codecs.rb
+++
@@ -1,6 +0,0 @@
-class ProAppsQuicktimeCodecs < Cask
-  url 'http://support.apple.com/downloads/DL1396/en_US/ProAppsQTCodecs.dmg'
-  homepage 'http://support.apple.com/kb/dl1396'
-  version '1.0.3'
-  sha256 :no_check
-end
