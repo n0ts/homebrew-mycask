c488dffd44a718c07aecb0bfe9d1eb69dcd1ddfa
n0ts <me@n0ts.org>
2014-08-09 01:22:55 +0900

    Add sha256

--- Casks/wondershare-player.rb
+++ Casks/wondershare-player.rb
@@ -2,6 +2,6 @@ class WondersharePlayer < Cask
   url 'http://download.wondershare.com/mac-player_full1479.dmg'
   homepage 'http://www.wondershare.com/video-player/'
   version 'latest'
-  sha256 :no_check
+  sha256 '8eb2172b5086f58a0db3535e11198cb3dac9f213204f512f36cfd6c82c2629b0'
   link 'Wondershare Player.app'
 end
