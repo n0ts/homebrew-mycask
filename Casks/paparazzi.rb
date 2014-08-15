6e721cff64b192b35217225c93e8eea671f2100c
n0ts <me@n0ts.org>
2014-08-15 10:47:05 +0900

    Duplicate official homebrew-cask

--- Casks/paparazzi.rb
+++
@@ -1,7 +0,0 @@
-class Paparazzi < Cask
-  url 'http://derailer.org/paparazzi/Paparazzi!%200.6.7.dmg'
-  homepage 'http://derailer.org/paparazzi/'
-  version '0.6.7'
-  sha256 :no_check
-  link 'Paparazzi!.app'
-end
