ffefd60d932bfa18f41371ce3dadee3247424328
n0ts <me@n0ts.org>
2014-08-15 10:44:19 +0900

    Duplicate official homebrew-cask

--- Casks/arranger.rb
+++
@@ -1,7 +0,0 @@
-class Arranger < Cask
-  url 'http://bucketomac.de/downloads/Arranger1.4.dmg'
-  homepage 'http://bucketomac.de/arranger/arranger/'
-  version '1.4'
-  sha256 :no_check
-  link 'Arranger.app'
-end
