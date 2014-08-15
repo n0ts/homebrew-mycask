c3f04f419cac4a4b95decf68b8641d7ecc6eccc0
n0ts <me@n0ts.org>
2014-08-15 10:50:30 +0900

    Add sha256 checksum

--- Casks/iphone-config-utility.rb
+++ Casks/iphone-config-utility.rb
@@ -1,7 +1,9 @@
 class IphoneConfigUtility < Cask
+  version '3.5'
+  sha256 '822204947ae5f7739bcb1e9694814f8e3ef65cd184952b76104f525d97a28163'
+
   url 'http://support.apple.com/downloads/DL1465/en_US/iPhoneConfigUtility.dmg'
   homepage 'http://support.apple.com/downloads/#iphone'
-  version '3.5'
-  sha256 :no_check
+
   install 'iPhoneConfigurationUtility.pkg'
 end
