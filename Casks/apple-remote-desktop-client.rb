a9cf08255a363724072c1eac01d64c8ca6d9287f
n0ts <me@n0ts.org>
2014-08-15 10:34:28 +0900

    Add sha256 Apple Remote Desktop Client

--- Casks/apple-remote-desktop-client.rb
+++ Casks/apple-remote-desktop-client.rb
@@ -1,7 +1,9 @@
 class AppleRemoteDesktopClient < Cask
+  version '3.7.1'
+  sha256 '36ae71ea8259e040743061a8aebe7b6f69eb1e8d1b235dfbc52420eb98e27f5a'
+
   url 'http://support.apple.com/downloads/DL1710/en_US/RemoteDesktopClient371.dmg'
   homepage 'http://support.apple.com/kb/dl1710'
-  version '3.7.1'
-  sha256 :no_check
+
   install 'RemoteDesktopClient.pkg'
 end
