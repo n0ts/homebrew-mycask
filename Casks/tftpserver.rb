25ba691fc77627cdf3264af8cc7d076dd61446b1
n0ts <me@n0ts.org>
2014-08-15 10:54:11 +0900

    Change version latest

--- Casks/tftpserver.rb
+++ Casks/tftpserver.rb
@@ -1,7 +1,9 @@
 class Tftpserver < Cask
+  version 'latest'
+  sha256 :no_check
+
   url 'http://ww2.unime.it/flr/tftpserver/TftpServer.dmg'
   homepage 'http://ww2.unime.it/flr/tftpserver/'
-  version '3.4.1'
-  sha256 :no_check
+
   link 'TftpServer.app'
 end
