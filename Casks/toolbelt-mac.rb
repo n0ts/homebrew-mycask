8cdf8d1686656fd8cf7b17fcc4533e9f31b4441e
n0ts <me@n0ts.org>
2014-08-15 12:55:31 +0900

    Rename td-toolbelt

--- Casks/toolbelt-mac.rb
+++
@@ -1,7 +0,0 @@
-class ToolbeltMac < Cask
-  url 'https://d3hq3slxjftf62.cloudfront.net/td-toolbelt-0.11.1.pkg'
-  homepage 'http://toolbelt.treasuredata.com/'
-  version '0.11.1'
-  sha256 :no_check
-  install 'td-toolbelt-0.11.1.pkg'
-end
