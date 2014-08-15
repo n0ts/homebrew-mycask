d60d61408b64b1e32d3bfd5fb2eabe9a1cdf538a
n0ts <me@n0ts.org>
2014-08-15 16:27:11 +0900

    Merge official homebrew-cask https://github.com/caskroom/homebrew-cask/pull/5781

--- Casks/mac-zip4-win.rb
+++
@@ -1,9 +0,0 @@
-class MacZip4Win < Cask
-  version '0.4.1'
-  sha256 '169e84b66dfd2d513385c86726a8c78c37cf73c8c2bf753ddb6fd112d7b38ec7'
-
-  url 'http://ynomura.com/files/MacZip4win_0_4_1.zip'
-  homepage 'http://ynomura.com/home/?page_id=116'
-
-  link 'MacZip4win 0.4.1/MacZip4Win.app'
-end
