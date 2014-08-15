7b23f6bcc1ace53adb1445e9298f571ea6ead76a
n0ts <me@n0ts.org>
2014-08-15 10:45:05 +0900

    Duplicate official homebrew-cask

--- Casks/nike-plus-connect.rb
+++
@@ -1,7 +0,0 @@
-class NikePlusConnect < Cask
-  url 'http://nikeplus.nike.com/nikeplus/nikeconnect/installers/macosx/Nike+Connect_new.dmg'
-  homepage 'https://secure-nikeplus.nike.com/plus/'
-  version 'latest'
-  sha256 :no_check
-  install 'Nike+Connect_new.pkg'
-end
