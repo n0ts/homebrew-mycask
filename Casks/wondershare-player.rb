f4bfdf4c38b05aa7497cc87f937e0d7c9e776e4d
n0ts <me@n0ts.org>
2014-08-15 10:18:22 +0900

    wondershare-player move to official repository

--- Casks/wondershare-player.rb
+++
@@ -1,7 +0,0 @@
-class WondersharePlayer < Cask
-  url 'http://download.wondershare.com/mac-player_full1479.dmg'
-  homepage 'http://www.wondershare.com/video-player/'
-  version 'latest'
-  sha256 '8eb2172b5086f58a0db3535e11198cb3dac9f213204f512f36cfd6c82c2629b0'
-  link 'Wondershare Player.app'
-end
