395ac28d468f63ddb4b7b813add2829398cf7a96
n0ts <me@n0ts.org>
2014-08-21 16:56:01 +0900

    Refactoring

--- Casks/mercurial.rb
+++ Casks/mercurial.rb
@@ -1,9 +1,12 @@
 class Mercurial < Cask
-  url 'http://mercurial.selenic.com/mac/binaries/Mercurial-3.0.1-py2.7-macosx10.9.zip'
-  homepage 'http://mercurial.selenic.com/'
   version '3.0.1'
   sha256 :no_check
-  install 'mercurial-3.0.1_20140606-py2.7-macosx10.9/mercurial-3.0.1+20140606-py2.7-macosx10.9.mpkg'
+
+  url "http://mercurial.selenic.com/mac/binaries/Mercurial-#{versoin}-py2.7-macosx10.9.zip"
+  homepage 'http://mercurial.selenic.com/'
+  version '3.0.1'
+
+  install "mercurial-#{version}_20140606-py2.7-macosx10.9/mercurial-#{version}+20140606-py2.7-macosx10.9.mpkg"
   caveats do
     os_version_only '10.9'
   end
