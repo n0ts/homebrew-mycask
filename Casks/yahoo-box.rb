fd6b6ba041da0a9a2cb08155b37972fc1bd971b0
n0ts <me@n0ts.org>
2014-09-22 09:32:58 +0900

    Add Yahoo! box

---
+++ Casks/yahoo-box.rb
@@ -0,0 +1,8 @@
+# coding: utf-8
+class YahooBox < Cask
+  url 'http://rdsig.yahoo.co.jp/box/web/install_mac/evt=85149/RV=1/RU=aHR0cDovL2kueWltZy5qcC9kbC9ib3gvaW5zdGFsbHMveWJveF9tYWMuZG1n'
+  homepage 'http://info.box.yahoo.co.jp/'
+  version 'latest'
+  sha256 :no_check
+  link 'Yahoo!ボックス.app'
+end
