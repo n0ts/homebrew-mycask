3fee184e9a956322bba7a34c23e0c591b1a602cf
n0ts <naoya.n@gmail.com>
2015-02-13 17:11:50 +0900

    Add Kindle for Mac JP

---
+++ Casks/kindle-jp.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'kindle-jp' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.amazon.co.jp/kindlemacdownload'
+  homepage 'http://www.amazon.co.jp/gp/feature.html/?ie=UTF8&docId=3077089416'
+  license :unknown
+
+  container :type=> 'dmg'
+  app 'Kindle.app', :target => 'Kindle for Mac JP.app'
+end
