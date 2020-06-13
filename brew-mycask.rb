b5b4977b4438f2bc937744050a00fe1d6288d56c
n0ts <naoya.n@gmail.com>
2020-06-13 10:32:34 +0900

    Update lateste .

--- brew-mycask.rb
+++
@@ -1,8 +0,0 @@
-require 'formula'
-
-class BrewMycask < Formula
-  homepage 'https://github.com/n0ts/homebrew-mycask/'
-  url 'https://github.com/n0ts/homebrew-mycask.git'
-  def install
-  end
-end
