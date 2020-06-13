a5c3789429873720b23bc214915646cea011b220
n0ts <naoya.n@gmail.com>
2020-06-13 10:43:24 +0900

    Update homepage AppleRemoteDesktop

--- Casks/apple-remote-desktop-client.rb
+++ Casks/apple-remote-desktop-client.rb
@@ -3,7 +3,7 @@ cask 'apple-remote-desktop-client' do
   sha256 'f142c011487edc4541cbadbc1288a1685e06d78279c4bbf2a0cf44cd23b3fc30'
 
   url 'https://updates.cdn-apple.com/2019/cert/041-89063-20191011-38e81dab-6b55-495a-9992-7a1dea8dd5b9/RemoteDesktopClient.dmg'
-  homepage 'http://support.apple.com/kb/dl1710'
+  homepage 'https://support.apple.com/kb/DL1924'
 
   pkg 'RemoteDesktopClient.pkg'
 end
